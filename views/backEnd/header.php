b06efaf1d70f39b941e3b8ded03d089ad3289305
valdsuvorov <vladimir.suvorov@laposte.net>
2018-12-05 10:23:38 +0100

    Changement de redirection

--- views/backEnd/header.php
+++ views/backEnd/header.php
@@ -4,7 +4,7 @@
   ?>
   <ul>
   <li id="logo-li"><img id="logo" src="./public/images/logo.png"></li>
-  <li><a href="#accueil"><p>Accueil</p></a></li>
+  <li><a href="././Accueil"><p>Accueil</p></a></li>
   <li><a href="././register"><p>Inscription</p></a></li>
   <li><a href="././login"><p>Connexion</p></a></li>
   <li><a href="#aide"><p>Aide</p></a></li>
@@ -17,8 +17,8 @@ else
   ?>
     <ul>
   <li id="logo-li"><img id="logo" src="./public/images/logo.png"></li>
-  <li><a href="#accueil"><p>Accueil</p></a></li>
-  <li><a href="#inscription"><p>Gestion Profil</p></a></li>
+  <li><a href="././Accueil"><p>Accueil</p></a></li>
+  <li><a href="././Gestioncompte"><p>Gestion Profil</p></a></li>
   <li><a href="#connexion"><p>Gestion Maison</p></a></li>
   <li><a href="././logout"><p>Déconnexion</p></a></li>
   <li><a href="#contact"><p>Contactez-nous !</p></a></li>
