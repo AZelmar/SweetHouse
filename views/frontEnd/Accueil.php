2e0c131b1d409bfb195f3751469e5219019bc048
valdsuvorov <vladimir.suvorov@laposte.net>
2018-12-05 10:20:08 +0100

    Création Page Accueil

---
+++ views/frontEnd/Accueil.php
@@ -0,0 +1,71 @@
+<?php
+include 'backEnd/config.php'
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css" /> 
+    <title>SweetHouse</title>
+</head>
+
+<body> 
+<?php
+  include 'backEnd/header.php';
+  ?>
+<header>  
+<div class="titre">
+    <h1>La Maison du Futur</h1>
+    <p>Découvrez SweetHouse</p>
+</div>
+  </header>
+<main>
+  <h2>ATTENTION !</h2>
+  <p>Site bientôt disponible!</p>
+</main>
+</body>
+</html>
+<style>
+ 
+body {
+  text-align:center;
+    font-weight: normal;
+    font-style:normal;
+    background-size :cover;
+    cursor:auto;
+    padding: 0;
+	margin:0;
+}
+
+
+header {
+  background: url(http://yesofcorsa.com/wp-content/uploads/2018/06/Smart-House-Wallpaper-1080p-1024x576.jpg);
+  background-size: cover;
+  height: 100vh;
+}
+
+  
+.titre {
+  position: absolute;
+  top: 20%;
+  left: 70%;
+  color: #cffdf8;
+
+}
+  
+h1 {
+  margin: 0;
+  font-size: 5rem;
+}
+h2 {
+  font-size: 4rem;
+}
+p {
+  font-size: 1.5rem;
+}
+
+main {
+  max-width: 500px;
+  margin: 25px auto;
+
+</style>
