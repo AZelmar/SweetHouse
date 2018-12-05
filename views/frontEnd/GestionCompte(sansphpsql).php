9f978b71d8933b9294a5f6e408ff8499af438a15
valdsuvorov <vladimir.suvorov@laposte.net>
2018-12-05 10:15:42 +0100

    Update GestionCompte(sansphpsql).php
    Ajout de php au code

--- views/frontEnd/GestionCompte(sansphpsql).php
+++ views/frontEnd/GestionCompte(sansphpsql).php
@@ -1,49 +1,45 @@
+<?php 
+include 'backEnd/config.php';
+include 'model/userEntity.php';
+
+$resultat = getUserData();
+
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="style.css" /> 
 	<title>SweetHouse</title>
 </head>
-
-  <!DOCTYPE html>
+  
 <body>
-<header>
-
-	<ul>
-  <li id="logo-li"><img id="logo" src="logo.png"></li>
-  <li><a href="#accueil">Accueil</a></li>
-  <li><a href="#inscription">Inscription</a></li>
-  <li><a href="#connexion">Connexion</a></li>
-  <li><a href="#aide">Aide</a></li>
-  <li><a href="#contact" >Contactez-nous !</a></li>
-	</ul>
-
-</header>
-
-
+<?php include 'header.php' ?>
 	<div id="Profil">
-
 	<h2 id="title">Gestion du profil</h2>
 		<hr>
+      <form method="post" action="mod_profile.php">
+        <p>
 		<div class="input-group">
 			<label> Modifier votre adresse de domicile :</label>
-			<input type="text">
+			<input type="text" name="adress" value= "<? echo $resultat['adress'] ?>" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre adresse bureau:</label>
-			<input type="text">
+			<input type="text" name="adressbur" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre nom:</label>
-			<input type="text">
+			<input type="text" name="last-name" value= "<? echo $resultat['lastName'] ?>" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre prénom:</label>
-			<input type="text">
+			<input type="text" name="first-name" value= "<? echo $resultat['firstName'] ?>" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre sexe:</label>
-			<input type="text">
+			<input type="text" name="gender" value= "<? echo $resultat['gender'] ?>" >
 		</div>
 		<div class="input-group">
 			<label>Contacter son technicien:</label>
@@ -51,18 +47,21 @@
 				   value="Pierre Louis, 0684759562, pierrelouis158@gmail.com"
 				   size="50">
 		</div>
-
-	</div>
-
+       <input type="submit" name="valider" value="modifier"/> 
+	</div>	
+      
+    <div id="aside">
+      
+      
 	<div id="Infos">
 	<h2 id="title">Informations générales</h2>
 		<hr>
 		<div class="infos-group">
-			<h1 id="titreinfos"> Bonjour Vladimir </h1>
+			<h1 id="titreinfos"> Bonjour <? echo $resultat['firstName'] ?> </h1>
 			<p id="texteinfos"> 
 				Vos lieux domotisés: 
 				<br><br>
-				 Maison : 
+				 Maison: <? echo $resultat['adress'] ?>  
 				<br><br> 
 				Entreprise:
 				<br><br><br>
@@ -88,71 +87,6 @@ body{
 	background-color:#cffdf8
 }
 
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #f76262;
-    display: flex;
-    position:relative;
-    z-index:8;
-
-}
-
-
-li {
-    float: left;
-    display:flex;
-    align-items: center;
-    border-left: 1px dotted black;
-    
-}
-li:last-child {
-    margin-left: auto;
-    
-}
-li:nth-last-child(2) {
-    border-right: 1px dotted black;
-
-}
-
-li a {
-    color: #cffdf8;
-    padding: 4px 16px;
-    text-decoration: none;
-    font-size: 1.5em;
-    font-family: Lato, Helvetica, sans-serif;
-    border-radius:10px;
-}
-
-li:hover:not(.active) {
-    background-color: #216583;
-    transition: all 1s;
-    
-}
-
-li a:focus {
-    border : 3px dotted white;
-}
- li:a:active{
-    color: #f76262;
- }
-
-
-#logo-li {
-    border-right: none;
-}
-#logo{
-    display: block;
-    height: 10vh;
-    width: 10vw;
-}
-.active {
-    background-color: #D3D3D3;
-}
-
-
 #title{
 	text-align: center;
 }
@@ -171,17 +105,18 @@ hr{
 	float:right;
 	margin-right: 200px;
 	border: 3px solid black;
-	padding: 10px;
+	padding: 20px;
 	margin-top: 30px;
 	background-color: #216583;
 	color:#cffdf8;
 	width: 35%;
 
+
 }
 
 #Infos {
 	float:left;
-	margin-left: 300px;
+	margin-left: 200px;
 	border: 3px solid black;
 	padding: 10px;
 	margin-top: 30px;
@@ -196,8 +131,21 @@ hr{
 	margin-top: -30px;
 }
 
-.texteinfos{
+input[type=submit] {
+  	width: 100%;
+  	background-color: #f76262;
+  	color: #cffdf8;
+  	padding: 14px 20px;
+  	margin: 10px 0;
+  	border: 3px solid black;
+  	border-radius: 10px;
+  	cursor: pointer;
+}
 
+input[type=submit]:hover {
+  background-color: #65c0ba;
+  transition-duration: 0.4s;
+  color: #216583;
 }
 
 </style>
