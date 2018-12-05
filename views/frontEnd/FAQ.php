13d3e1b03723be1e6ce100d9ad517afdba42c1e3
LpineA <45098756+LpineA@users.noreply.github.com>
2018-12-05 09:33:23 +0100

    Version 1.1 FAQ
    - Fixing "header" problems
    - Improving FAQ style

--- views/frontEnd/FAQ.php
+++ views/frontEnd/FAQ.php
@@ -1,17 +1,14 @@
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style_FAQ.css">
+    <title>SweetHouse | Foire aux questions</title>
     <meta charset="UTF-8">
-    <link rel="./public/css/style.css">
 </head>
 <body>
 
 <?php
     include "header.php";
-    /*$notification  = array('type' => "error", "message"=> "Error");
-    include "notification.php";*/
 ?>
 
 <h1><div class="titre-question">
@@ -58,6 +55,25 @@
 
 <style>
 
+    h1{
+        text-align: center;
+        font-family: Lato, Helvetica, sans-serif;
+        text-align: center;
+        font-weight: lighter;
+        margin-bottom: 3%;
+
+    }
+
+    .presentation-questions{
+        margin-left: 5%;
+        margin-right: 5%;
+        font-family: Lato, Helvetica, sans-serif;
+        text-align: justify;
+        margin-bottom: 3%;
+
+
+
+    }
 body {
 background-color: #cffdf8;
 font-weight: normal;
@@ -96,8 +112,10 @@ text-align: left;
 outline: none;
 font-size: 15px;
 width: 80%;
-margin-left: 5%;
-margin-right: 5%;
+    margin-left: 10%;
+    font-family: Lato, Helvetica, sans-serif;
+font-weight: lighter;
+    font-size: 1.1em;
 
 }
 
@@ -110,7 +128,7 @@ content: '\002B';
 color: white;
 font-weight: bold;
 float: right;
-margin-left: 5px;
+margin-left: 10px;
 }
 
 .active:after {
@@ -123,10 +141,113 @@ max-height: 0;
 overflow: hidden;
 transition: max-height 0.2s ease-out;
 background-color: #f1f1f1;
-width: 80%;
-margin-left: 5%;
-margin-right: 5%;
+width: 77.5%;
+margin-left: 10%;
+    font-family: Lato, Helvetica, sans-serif;
+    font-weight: lighter;
+    font-size: 0.92em;
 }
+
+
+
+body {
+    background-color: #cffdf8;
+    font-weight: normal;
+    font-style:normal;
+    background-size :cover;
+    cursor:auto;
+    padding: 0;
+    margin:0;
+
+}
+
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #f76262;
+    display: flex;
+}
+
+
+li {
+    float: left;
+    display: flex;
+    align-items: center;
+    border-left: 1px solid black;
+}
+li:last-child {
+    margin-left: auto;
+}
+li:nth-last-child(2) {
+    border-right: 1px solid black;
+}
+
+li a {
+    color: black;
+    padding: 4px 16px;
+    text-decoration: none;
+    font-size: 1.5em;
+    font-family: Lato, Helvetica, sans-serif;
+    border-radius:10px;
+    height: 100%;
+}
+li a p {
+    vertical-align: middle;
+}
+li:hover:not(.active) {
+    background-color: #216583;
+    transition: all 1s;
+}
+
+li a:focus {
+    border: 0;
+}
+li:a:active{
+    color: #f76262;
+}
+
+
+#logo-li {
+    border-right: none;
+}
+#logo{
+    display: block;
+    height: 10vh;
+    width: 10vw;
+}
+.active {
+    background-color: #D3D3D3;
+}
+.notification{
+    margin: 10px 0px;
+    padding:12px;
+    width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
+.notification_error{
+    color: #D8000C;
+    background-color: #FFD2D2;
+}
+.notification_success{
+    color: #4F8A10;
+    background-color: #DFF2BF;
+}
+#validate-button{
+    margin:0 auto;
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 9px 24px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 14px;
+}
+
 
 </style>
 
