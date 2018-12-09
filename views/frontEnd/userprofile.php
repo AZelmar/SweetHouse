2418645ce4f3a3178e958d2ced90b0cc1e1fa663
valdsuvorov <vladimir.suvorov@laposte.net>
2018-12-09 10:21:41 +0100

    version finale de la gestion profil

--- views/frontEnd/userprofile.php
+++ views/frontEnd/userprofile.php
@@ -1,29 +1,30 @@
-<?php 
-include 'backEnd/config.php';
-include 'model/userEntity.php';
-
-$resultat = getUserData();
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="style.css" /> 
+	<link rel="stylesheet" href="./public/css/style.css" /> 
 	<title>SweetHouse</title>
 </head>
-  
+<?php 
+include 'controller/userController.php';
+include 'backEnd/config.php';
+include 'model/userEntity.php';
+include 'backEnd/header.php';
+$resultat = getUserData();
+$email=$_SESSION['email'];
+
+?>
+
 <body>
-<?php include 'backEnd/header.php' ?>
 	<div id="Profil">
 	<h2 id="title">Gestion du profil</h2>
-		<hr>
-      <form method="post" action="mod_profile.php">
+      <hr>
+      <form method="post" action="userprofile">
         <p>
+        
 		<div class="input-group">
 			<label> Modifier votre adresse de domicile :</label>
-			<input type="text" name="adress" value= "<? echo $resultat['adress'] ?>" >
+			<input type="text" name="adress" value= "<? echo htmlentities($resultat['adress']) ?>" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre adresse bureau:</label>
@@ -31,15 +32,15 @@ $resultat = getUserData();
 		</div>
 		<div class="input-group">
 			<label>Modifier votre nom:</label>
-			<input type="text" name="last-name" value= "<? echo $resultat['lastName'] ?>" >
+			<input type="text" name="last-name" value= "<? echo htmlentities($resultat['lastName']) ?>" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre prénom:</label>
-			<input type="text" name="first-name" value= "<? echo $resultat['firstName'] ?>" >
+			<input type="text" name="first-name" value= "<? echo htmlentities($resultat['firstName']) ?>" >
 		</div>
 		<div class="input-group">
 			<label>Modifier votre sexe:</label>
-			<input type="text" name="gender" value= "<? echo $resultat['gender'] ?>" >
+			<input type="text" name="gender" value= "<? echo htmlentities($resultat['gender']) ?>" >
 		</div>
 		<div class="input-group">
 			<label>Contacter son technicien:</label>
@@ -47,21 +48,39 @@ $resultat = getUserData();
 				   value="Pierre Louis, 0684759562, pierrelouis158@gmail.com"
 				   size="50">
 		</div>
-       <input type="submit" name="valider" value="modifier"/> 
-	</div>	
-      
+       <input type="submit" name="valider" value="modifier"/>
+      	</form>
+  </div>
+    
     <div id="aside">
-      
-      
+<?php
+  
+ if (isset($_POST['valider']) )
+{
+  if ((!empty($_POST['adress'])) && (!empty($_POST['gender'])) && (!empty($_POST['first-name'])) && (!empty($_POST['last-name'])))
+     {
+          $req= 'UPDATE user SET firstName = :firstName, lastName = :lastName, gender =:gender, adress =:adress 
+          		WHERE email = "'.$email.'"';
+          $reqp= $pdo->prepare($req);
+          $reqp ->execute(array('lastName'=>$_POST['last-name'],'firstName'=>$_POST['first-name'],'gender'=>$_POST['gender'], 'adress'=>$_POST['adress']));
+         }
+  else
+  {
+    echo 'Vous devez remplir tous les champs !';
+  }
+}
+                   
+?>
+      </div>    
 	<div id="Infos">
 	<h2 id="title">Informations générales</h2>
 		<hr>
 		<div class="infos-group">
-			<h1 id="titreinfos"> Bonjour <? echo $resultat['firstName'] ?> </h1>
+			<h1 id="titreinfos"> Bonjour <? echo htmlentities($resultat['firstName']) ?> </h1>
 			<p id="texteinfos"> 
 				Vos lieux domotisés: 
 				<br><br>
-				 Maison: <? echo $resultat['adress'] ?>  
+				 Maison: <? echo htmlentities($resultat['adress']) ?>  
 				<br><br> 
 				Entreprise:
 				<br><br><br>
@@ -107,8 +126,8 @@ hr{
 	border: 3px solid black;
 	padding: 20px;
 	margin-top: 30px;
-	background-color: #216583;
-	color:#cffdf8;
+	background-color: #65c0ba;
+	color: black;
 	width: 35%;
 
 
@@ -120,8 +139,8 @@ hr{
 	border: 3px solid black;
 	padding: 10px;
 	margin-top: 30px;
-	background-color: #216583;
-	color:#cffdf8;
+	background-color: #65c0ba;
+	color:black;
 	width: 30%;
 	font-size: 30px;
 }
@@ -133,8 +152,8 @@ hr{
 
 input[type=submit] {
   	width: 100%;
-  	background-color: #f76262;
-  	color: #cffdf8;
+  	background-color: #216583;
+  	color: black;
   	padding: 14px 20px;
   	margin: 10px 0;
   	border: 3px solid black;
@@ -143,9 +162,9 @@ input[type=submit] {
 }
 
 input[type=submit]:hover {
-  background-color: #65c0ba;
+  background-color: white;
   transition-duration: 0.4s;
-  color: #216583;
+  color: black;
 }
 
 </style>
