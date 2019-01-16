01884882571638618eaafec97090e286792093b6
hamzaaitomar <hamza.aitomar@gmail.com>
2019-01-16 10:39:16 +0100

    Create technicianEntity.php

---
+++ model/technicianEntity.php
@@ -0,0 +1,25 @@
+<?php
+
+function getTechnicianData($email)
+
+{
+  require("config.php");
+  $request = $pdo->prepare('SELECT * FROM user WHERE email= ? ');
+  $request->execute(array($email));
+  $result = $request->fetch();
+  return $result;
+  
+}
+
+function getTechnicianRDV($id)
+
+{
+  require("config.php");
+  $request = $pdo->prepare('SELECT * FROM technicianRDV WHERE technicianId= ? ');
+  $request->execute(array($id));
+  $result1 = $request->fetchAll();
+  return $result1;
+  
+}
+
+?>
