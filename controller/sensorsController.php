69a6c870f35e461d130375acf4ce1966aba4f8cb
hamzaaitomar <hamza.aitomar@gmail.com>
2019-01-16 10:48:37 +0100

    Update sensorsController.php

--- controller/sensorsController.php
+++ controller/sensorsController.php
@@ -6,3 +6,16 @@
  * Time: 13:29
  */
 
+function gestion_capteurs($locale){
+
+    if(isConnected($locale)) {
+        require ('../views/frontEnd/GestionCapteurs.php');
+
+       var_dump($_POST['#tab1']);
+
+        if($'#tab1' = $_GET['chambres']){
+          echo "ceci est ma variable javascript dans du code PHP : ".$chambres;
+       }
+       else{
+          echo 'didier';
+       
